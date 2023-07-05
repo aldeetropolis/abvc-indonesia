8d7bd7ee7243774ce2c82a993e3cb28a5f88739a
Aldilas Achmad Nursetyo <aldee.naruto@gmail.com>
2023-07-05 17:11:26 +0700

    Update 5 jul 2023

--- covid-19 pubmed search.R
+++ covid-19 pubmed search.R
@@ -1,10 +1,12 @@
 library(RISmed)
 library(writexl)
+library(DT)
 
 topic <- "covid-19"
-query <- EUtilsSummary(topic, retmax = 100, mindate = "2023-06-28", maxdate = "2023-06-30")
+query <- EUtilsSummary(topic, type = "esearch", db = "pubmed", retmax = 100, reldate = 6)
 summary(query)
 
 records <- EUtilsGet(query)
 data <- data.frame("Title"=ArticleTitle(records),"Abstract"=AbstractText(records), "PMID"=PMID(records))
-write_xlsx(data, "pubmed covid-19 30 june 2023.xlsx")
+#write_xlsx(data, "pubmed covid-19 30 june 2023.xlsx")
+datatable(data)
