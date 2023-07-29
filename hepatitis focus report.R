531172f193828a3ff2d2d047a947fdf246ced169
aldeetropolis <aldee.naruto@gmail.com>
2023-07-29 21:53:11 +0700

    maps

--- hepatitis focus report.R
+++ hepatitis focus report.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(maps)
 library(mapdata)
+library(readxl)
 
+data_hbv <- read_xlsx("")
 
 map <- map_data("world", c("Vietnam", "Indonesia", "Malaysia", "Singapore", "Cambodia", "Laos", "Brunei", "Philippines", "Thailand", "Myanmar"))
 ggplot(map, aes(long, lat, group = group)) + geom_polygon(fill = "white", colour = "grey80") +
