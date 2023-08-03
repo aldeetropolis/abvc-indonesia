e4dd8ab36f11243ff10b685b02efb0e1184a5494
Aldilas Achmad Nursetyo <aldee.naruto@gmail.com>
2023-08-03 16:42:24 +0700

    COVID-19 3 august 2023

--- asean_map.R
+++ asean_map.R
@@ -7,15 +7,10 @@ map <- map_data("world", c("Vietnam", "Indonesia", "Malaysia", "Singapore", "Cam
 ggplot(map, aes(long, lat, group = group)) + geom_polygon(fill = "white", colour = "grey80") +
   coord_quickmap()
 
-<<<<<<< HEAD
-world <- map_data("world")
-
 data_hbv <- read_xlsx("HBV seroprevalence.xlsx") |> 
   filter(Region %in% c("Brunei Darussalam", "Cambodia", "Indonesia", "Laos", "Philippines", "Malaysia", "Myanmar", "Thailand", "Vietnam", "Singapore"))
 
 data_hcv <- read_xlsx("HCV seroprevalence.xlsx") |> 
   filter(Region %in% c("Brunei Darussalam", "Cambodia", "Indonesia", "Laos", "Philippines", "Malaysia", "Myanmar", "Thailand", "Vietnam", "Singapore"))
 
-=======
 world <- map_data("world")
->>>>>>> 10904b18c7c51dc0dc5888652ede647edde41f7d
