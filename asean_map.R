e4ff7148733bac51f0372677e2f1ebcbce31693b
Aldilas Achmad Nursetyo <aldee.naruto@gmail.com>
2023-08-03 08:19:23 +0700

    asean map

--- asean_map.R
+++ asean_map.R
@@ -1,10 +1,17 @@
 library(tidyverse)
 library(maps)
 library(mapdata)
-
+library(readxl)
 
 map <- map_data("world", c("Vietnam", "Indonesia", "Malaysia", "Singapore", "Cambodia", "Laos", "Brunei", "Philippines", "Thailand", "Myanmar"))
 ggplot(map, aes(long, lat, group = group)) + geom_polygon(fill = "white", colour = "grey80") +
   coord_quickmap()
 
 world <- map_data("world")
+
+data_hbv <- read_xlsx("HBV seroprevalence.xlsx") |> 
+  filter(Region %in% c("Brunei Darussalam", "Cambodia", "Indonesia", "Laos", "Philippines", "Malaysia", "Myanmar", "Thailand", "Vietnam", "Singapore"))
+
+data_hcv <- read_xlsx("HCV seroprevalence.xlsx") |> 
+  filter(Region %in% c("Brunei Darussalam", "Cambodia", "Indonesia", "Laos", "Philippines", "Malaysia", "Myanmar", "Thailand", "Vietnam", "Singapore"))
+
