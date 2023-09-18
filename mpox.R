a672db89746489397ed3ff1d43127f3f48e90141
aldeetropolis <aldee.naruto@gmail.com>
2023-09-18 11:07:18 +0700

    Update

--- mpox.R
+++ mpox.R
@@ -2,21 +2,10 @@ library(tidyverse)
 library(rio)
 library(readxl)
 
-data_mpox <- read_csv("mpox cases and deaths by country (17).csv")
-data_viet <- read_excel("VIetnam death case vaccination 2021-2022.xlsx") |> 
-  select(!c("...19", "...20", "sources")) |> 
-  mutate(reportedDate = openxlsx::convertToDate(reportedDate)) |> 
-  rename(weeklyDeaths = "weekly deaths",
-         cumulativeVaccination = "cumulative vaccination") |> 
-  filter(!is.na(cumulativeVaccination), !is.na(totalConfirmedCases))
+data <- read_csv("owid-monkeypox-data.csv") |> filter(date >= "2023-01-01") |> 
+  mutate(newCaseMA = round(zoo::rollmean(new_cases, k = 7, fill = NA), 2))
 
-cor(data_viet$cumulativeVaccination, data_viet$weeklyDeaths, method = "spearman")
-cor(data_viet$cumulativeVaccination, data_viet$totalDeaths)
+data_asean <- data |> 
+  filter(location %in% c("Malaysia", "Philippines", "Singapore", "Thailand", "Vietnam"))
 
-with(data_viet, plot(cumulativeVaccination, totalDeaths))
-abline(fit <- lm(cumulativeVaccination ~ totalDeaths, data = data_viet), col = 'red')
-legend("topright", bty="n", legend=paste("R2 =", format(summary(fit)$adj.r.squared, digits=4)))
-
-cor(data_viet$cumulativeVaccination, data_viet$totalConfirmedCases)
-t.test(data_viet$cumulativeVaccination, data_viet$weeklyDeaths)
-t.test(data_viet$cumulativeVaccination, data_viet$weeklyDeaths)
+ggplot(data |> filter(location == "World"), aes(x = date, y = newCaseMA)) + geom_line()
