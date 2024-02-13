909e1b44e00ee37d7cfd5c68fcda734278caf596
aldeetropolis <aldee.naruto@gmail.com>
2024-02-13 08:14:16 +0700

    Influenza Flunet

--- influenza-like illness.R
+++ influenza-like illness.R
@@ -6,18 +6,26 @@ library(zoo)
 library(jsonlite)
 library(httr)
 library(highcharter)
+library(wesanderson)
 
 infUrl <- "https://developer.bluedot.global/casecounts/indicator-based/diseases/influenza/?locationIds=1880251,%201605651,%201694008,%201820814,%201733045,%201643084,%201831722,%201327865,%201655842,1562822&startDate=2023-01-01&api-version=v1"
 res <- GET(infUrl, add_headers("Ocp-Apim-Subscription-Key" = "3f8e179c7c584514aa97faff3187df7d", "Cache-Control" = "no-cache")) |> content()
 infData <- enframe(pluck(res, "data")) |> unnest_wider(value)
 
-df_type <- data |> 
-  filter(country_code %in% c("BRN", "KHM", "IDN", "LAO", "PHL", "THA", "VNM", "MMR", "SGP", "MYS"),
-         iso_sdate >= "2023-01-01") |> 
-  rename(country = "country/area/territory") |> 
-  mutate(date = as.Date(iso_sdate)) |> 
-  select(country, date, inf_a, inf_b) |> 
+df_type_asean <- infData |> 
+  mutate(date = as.Date(reportedDate)) |> 
+  group_by(date) |> 
+  summarise(inf_a = sum(totalPositiveSamplesA, na.rm = TRUE),
+            inf_b = sum(totalPositiveSamplesB, na.rm = TRUE),
+            inf_tot = sum(totalPositiveSamples, na.rm = TRUE)) |> 
   pivot_longer(cols = c(inf_a, inf_b), names_to = "type", values_to = "n")
+
+df_type_country <- infData |> 
+  mutate(date = as.Date(reportedDate)) |> 
+  group_by(date, countryName,) |> 
+  summarise(inf_a = sum(totalPositiveSamplesA),
+            inf_b = sum(totalPositiveSamplesB)) |> 
+  pivot_longer(cols = c(inf_a, inf_b, inf_tot), names_to = "type", values_to = "n")
 
 df <- data |> 
   filter(country_code %in% c("BRN", "KHM", "IDN", "LAO", "PHL", "THA", "VNM", "MMR", "SGP", "MYS"),
@@ -38,9 +46,11 @@ plot <- ggplot() +
   theme(legend.title = element_blank()) +
   theme_hc()
 
-ggplot() + 
-  geom_col(data = filter(df_type, country == "Indonesia"), aes(date, as.numeric(n), fill = type)) +
-  geom_line(data = filter(df, country == "Indonesia"), aes(x = date, y = pos_rate_scaled))
+ggplot(data = df_type_asean) + 
+  geom_col(aes(date, as.numeric(n), fill = type),
+           position = position_dodge()) +
+  geom_line(aes(x = date, y = inf_tot, colour = "Total Cases")) +
+  scale_color_manual(values = "black")
 
 ggplot() +
   geom_smooth(data = filter(df, country == "Indonesia"), aes(x = date, y = pos_rate))
