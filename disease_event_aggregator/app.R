3f75d9710caf4618610986e0366292a6bb24d848
aldeetropolis <aldee.naruto@gmail.com>
2024-02-24 11:22:55 +0700

    Komponen Event-based Surveillance

--- disease_event_aggregator/app.R
+++ disease_event_aggregator/app.R
@@ -11,6 +11,7 @@ library(bs4Dash)
 library(httr)
 library(jsonlite)
 library(tidyverse)
+library(gt)
 
 other_country <- "2077456,1814991,1819730,102358,1835841,1668284,290557,2635167,1861060,1821275,289688,337996,1269750,2750405,2186224,286963,1168579,2017370,1227603,1966436,298795,1210997,2921044,130758,99237,248816,1522867,1282028,934292,1282988,6252001,1512440,290291,6251999,4043988,285570,1559582,2088628,1252634,2623032,2205218,660013,3017382,390903,3175395,798544,953987,2510769,2661886,2658434,2782113,2802361,357994,294640,192950,2029969,3144096,935317,1218197,690791"
 ams <- "1880251,1605651,1694008,1820814,1733045,1643084,1831722,1327865,1655842,1562822"
@@ -20,6 +21,7 @@ ui <- dashboardPage(
   dashboardHeader(title = "Disease Event"),
   dashboardSidebar(
     sidebarMenu(
+      menuItem("Human Cases & Deaths", tabName = "casedeath", icon = icon("th")),
       menuItem("Bluedot Newsfeed", tabName = "newsfeed", icon = icon("th")),
       menuItem("Event Alert", tabName = "event", icon = icon("th"))
       )
@@ -35,7 +37,7 @@ ui <- dashboardPage(
                             )),
                 dateInput("start_date", "Start Date:")),
               fluidRow(actionButton("run", "Run")),
-              fluidRow(dataTableOutput('table'))
+              fluidRow(gt_output(outputId = "table"))
               ),
       tabItem(tabName = "event")
     )
@@ -49,11 +51,12 @@ server <- function(input, output) {
     disease <- "4,6,7,10,13,15,19,37,38,39,40,41,43,47,53,55,62,64,83,84,88,11,113,115,136,141,153,156,164,166,172,174,200,202,203,204,205,206,207,212,215,216,224,239,264,275,281"
     url <- paste0("https://developer.bluedot.global/daas/articles/infectious-diseases/?startDate=", startDate, "&locationIds=", varCountry,"&diseaseId=", disease, "&includeBody=false&includeDuplicates=false&excludeArticlesWithoutEvents=false&limit=1000&format=json&api-version=v1")
     res <- content(GET(url, add_headers("Ocp-Apim-Subscription-Key" = "ae0e017fd9b9419a927a00f3f1524edb", "Cache-Control" = "no-cache")))
-    data <- enframe(pluck(res, "data")) |> unnest_wider(value) |> select(sourceUrl, sourceName, publishedTimestamp, articleHeadline, articleSummary) |> as_tibble()
+    data <- enframe(pluck(res, "data")) |> unnest_wider(value) |> unnest(diseases) |> unnest(locations) |> 
+      select(sourceUrl, sourceName, publishedTimestamp, diseases, locations, articleHeadline, articleSummary) |> as_tibble()
     return(data)
     })
 
-  output$table <- renderDataTable(data())
+  output$table <- render_gt(data())
 }
 
 # Run the application 
--- ebs.R
+++ ebs.R
@@ -2,13 +2,16 @@ library(tidyverse)
 library(lubridate)
 library(jsonlite)
 library(httr)
+library(gt)
 
 country <- c(2077456, 1814991, 1819730, 102358, 1835841, 1668284, 290557, 2635167, 1861060, 1821275, 289688, 337996, 1269750, 2750405, 2186224, 286963, 1168579, 2017370, 1227603, 1966436, 298795, 1210997, 2921044, 130758, 99237, 248816, 1522867, 1282028, 934292, 1282988, 6252001, 1512440, 290291, 6251999, 4043988, 285570, 1559582, 2088628, 1252634, 2623032, 2205218, 660013, 3017382, 390903, 3175395, 798544, 953987, 2510769, 2661886, 2658434, 2782113, 2802361, 357994, 294640, 192950, 2029969, 3144096, 935317, 1218197, 690791)
 ams <- c(1880251, 1605651, 1694008, 1820814, 1733045, 1643084, 1831722, 1327865, 1655842,1562822)
-
+disease <- "4,6,7,10,13,15,19,37,38,39,40,41,43,47,53,55,62,64,83,84,88,11,113,115,136,141,153,156,164,166,172,174,200,202,203,204,205,206,207,212,215,216,224,239,264,275,281"
+ams <- "1880251,1605651,1694008,1820814,1733045,1643084,1831722,1327865,1655842,1562822"
 startDate <- "2023-12-31"
 endDate <- "2024-01-03"
 
+# Bluedot - Human Disease Case & Death
 global_ebs <- data.frame()
 for (i in country){
   url <- paste0("https://developer.bluedot.global/casecounts/locations/", i, "?startDate=", startDate, "&endDate=", endDate, "&isAggregated=false&includeSources=true&api-version=v1")
@@ -66,7 +69,33 @@ sources <- enframe(pluck(res, "data", 1, "sources"))
 join <- cbind(event, data, diseases, species, locations, sources)
 event <- rbind(event, join)
 
+# Bluedot - Newsfeed
+url <- paste0("https://developer.bluedot.global/daas/articles/infectious-diseases/?startDate=", startDate, "&locationIds=", ams,"&diseaseId=", disease, "&includeBody=false&includeDuplicates=false&excludeArticlesWithoutEvents=false&limit=1000&format=json&api-version=v1")
+res <- content(GET(url, add_headers("Ocp-Apim-Subscription-Key" = "ae0e017fd9b9419a927a00f3f1524edb", "Cache-Control" = "no-cache")))
+data <- enframe(pluck(res, "data")) |> unnest_wider(value) |> unnest(diseases) |> unnest(locations)
+gt(data)
 
+|> select(sourceUrl, sourceName, publishedTimestamp, articleHeadline, diseases, locations, articleSummary)
+diseaseName <- enframe(pluck(res, "data", 10, "diseases")) |> select(-name) |> unnest_wider(value)
+
+url <- paste0("https://developer.bluedot.global/daas/articles/infectious-diseases/?startDate=", startDate, "&locationIds=", ams,"&diseaseId=", disease, "&includeBody=false&includeDuplicates=false&excludeArticlesWithoutEvents=false&limit=1000&format=json&api-version=v1")
+res <- content(GET(url, add_headers("Ocp-Apim-Subscription-Key" = "ae0e017fd9b9419a927a00f3f1524edb", "Cache-Control" = "no-cache")))
+length_df <- length(res$data)
+event <- data.frame()
+for (i in 1:length_df) {
+  data <- enframe(pluck(res, "data")) |> unnest_wider(value) |> select(sourceUrl, sourceName, publishedTimestamp, articleHeadline, diseases, locations, articleSummary)
+  diseaseName <- diseaseName <- enframe(pluck(res, "data", i, "diseases")) |> select(-name) |> unnest_wider(value)
+  join <- cbind(data, diseaseName)
+  event <- rbind(event, join)
+}
+
+url <- "https://developer.bluedot.global/daas/lookup/articles/themes/?theme=INFECTIOUSDISEASES&api-version=v1"
+res <- content(GET(url, add_headers("Ocp-Apim-Subscription-Key" = "ae0e017fd9b9419a927a00f3f1524edb", "Cache-Control" = "no-cache")))
+data <- enframe(pluck(res, "data")) |> unnest_wider(value)
+
+data |> map(diseases) |> reduce(diseases)
+
+df <- unnest(data, diseases)
 
 
 
