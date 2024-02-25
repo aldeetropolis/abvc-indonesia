ef540eabdcef6d15335e0ff161d3fdc2a096abe0
aldeetropolis <aldee.naruto@gmail.com>
2024-02-25 15:44:12 +0700

    Try to incorporate Human Disease Case & Death but no success

--- disease_event_aggregator/app.R
+++ disease_event_aggregator/app.R
@@ -11,7 +11,7 @@ library(bs4Dash)
 library(httr)
 library(jsonlite)
 library(tidyverse)
-library(gt)
+library(DT)
 
 other_country <- "2077456,1814991,1819730,102358,1835841,1668284,290557,2635167,1861060,1821275,289688,337996,1269750,2750405,2186224,286963,1168579,2017370,1227603,1966436,298795,1210997,2921044,130758,99237,248816,1522867,1282028,934292,1282988,6252001,1512440,290291,6251999,4043988,285570,1559582,2088628,1252634,2623032,2205218,660013,3017382,390903,3175395,798544,953987,2510769,2661886,2658434,2782113,2802361,357994,294640,192950,2029969,3144096,935317,1218197,690791"
 ams <- "1880251,1605651,1694008,1820814,1733045,1643084,1831722,1327865,1655842,1562822"
@@ -19,44 +19,75 @@ ams <- "1880251,1605651,1694008,1820814,1733045,1643084,1831722,1327865,1655842,
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
   dashboardHeader(title = "Disease Event"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Human Cases & Deaths", tabName = "casedeath", icon = icon("th")),
-      menuItem("Bluedot Newsfeed", tabName = "newsfeed", icon = icon("th")),
-      menuItem("Event Alert", tabName = "event", icon = icon("th"))
-      )
-  ),
+  dashboardSidebar(disable = TRUE),
   dashboardBody(
-    tabItems(
-      tabItem(tabName = "newsfeed",
-              fluidRow(
-                selectInput("country", "Pilih region:",
-                            choices = c(
-                              "ASEAN" = ams,
-                              "Other country" = other_country
-                            )),
-                dateInput("start_date", "Start Date:")),
-              fluidRow(actionButton("run", "Run")),
-              fluidRow(gt_output(outputId = "table"))
-              ),
-      tabItem(tabName = "event")
+    fluidRow(
+      column(width = 2,
+        selectInput("country", "Pilih region:",
+                        choices = c("ASEAN" = ams, "Other country" = other_country)),
+        dateInput("start_date", "Start Date:"),
+        actionButton("run", "Update Data")),
+      column(width = 10,
+             tabBox(width = 12, height = "675px",
+               tabPanel("Newsfeed API",
+                        DTOutput(outputId = "table_newsfeed")),
+               tabPanel("Human Disease Case & Death",
+                        DTOutput(outputId = "table_ebs"))
+               )
+             )
+      )
     )
-  ))
+  )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  data <- eventReactive(input$run, {
+  data_tab1 <- reactiveVal(NULL)
+  data_tab2 <- reactiveVal(NULL)
+  
+  observeEvent(input$run, {
+    # Get data from Newsfeed API
     varCountry <- input$country
     startDate <- input$start_date
     disease <- "4,6,7,10,13,15,19,37,38,39,40,41,43,47,53,55,62,64,83,84,88,11,113,115,136,141,153,156,164,166,172,174,200,202,203,204,205,206,207,212,215,216,224,239,264,275,281"
     url <- paste0("https://developer.bluedot.global/daas/articles/infectious-diseases/?startDate=", startDate, "&locationIds=", varCountry,"&diseaseId=", disease, "&includeBody=false&includeDuplicates=false&excludeArticlesWithoutEvents=false&limit=1000&format=json&api-version=v1")
     res <- content(GET(url, add_headers("Ocp-Apim-Subscription-Key" = "ae0e017fd9b9419a927a00f3f1524edb", "Cache-Control" = "no-cache")))
-    data <- enframe(pluck(res, "data")) |> unnest_wider(value) |> unnest(diseases) |> unnest(locations) |> 
-      select(sourceUrl, sourceName, publishedTimestamp, diseases, locations, articleHeadline, articleSummary) |> as_tibble()
-    return(data)
-    })
-
-  output$table <- render_gt(data())
+    data1 <- enframe(pluck(res, "data")) |> unnest_wider(value) |> unnest(diseases) |> unnest(locations) |> 
+      mutate(date = as.Date(publishedTimestamp)) |> select(sourceUrl, sourceName, date, diseases, locations, articleHeadline, articleSummary) |> 
+      unnest_wider(diseases, names_sep = "_") |> unnest_wider(locations, names_sep = "_")
+    
+    # Get data from Human Disease Case & Death API
+    url <- paste0("https://developer.bluedot.global/casecounts/?diseaseIds=", disease, "&locationIds=", varCountry, "&startDate=", startDate, "&isAggregated=false&includeSources=true&api-version=v1")
+    res <- GET(url, add_headers("Ocp-Apim-Subscription-Key" = "52bd528ca9cd407394791ca418a7b409", "Cache-Control" = "no-cache")) |> content()
+    data2 <- enframe(pluck(res, "data")) |> unnest_wider(value) |> mutate(date = as.Date(reportedDate)) |> 
+      select(date, diseaseName, countryName, minSources) |> unnest(minSources) |> 
+      unnest_wider(minSources)
+    
+    # Update table
+    data_tab1(data1)
+    data_tab2(data2)
+  })
+  
+  output$table_newsfeed <- renderDT(data_tab1(),
+                           options = list(
+                             autoWidth = TRUE,
+                             columnDefs = list(
+                               list(width = "50px", targets = c(1)),
+                               list(width = "100px", targets = c(8,9))),
+                             pageLength = 10,
+                             scrollX = TRUE,
+                             scrollY = "500px"
+                           ))
+  
+  output$table_ebs <- renderDT(data_tab2(),
+                               options = list(
+                                 autoWidth = TRUE,
+                                 columnDefs = list(
+                                   list(width = "50px", targets = c(1)),
+                                   list(width = "100px", targets = c(8,9))),
+                                 pageLength = 10,
+                                 scrollX = TRUE,
+                                 scrollY = "500px"
+                                 ))
 }
 
 # Run the application 
--- ebs.R
+++ ebs.R
@@ -8,26 +8,15 @@ country <- c(2077456, 1814991, 1819730, 102358, 1835841, 1668284, 290557, 263516
 ams <- c(1880251, 1605651, 1694008, 1820814, 1733045, 1643084, 1831722, 1327865, 1655842,1562822)
 disease <- "4,6,7,10,13,15,19,37,38,39,40,41,43,47,53,55,62,64,83,84,88,11,113,115,136,141,153,156,164,166,172,174,200,202,203,204,205,206,207,212,215,216,224,239,264,275,281"
 ams <- "1880251,1605651,1694008,1820814,1733045,1643084,1831722,1327865,1655842,1562822"
-startDate <- "2023-12-31"
-endDate <- "2024-01-03"
+startDate <- "2023-02-18"
+endDate <- "2024-02-24"
 
 # Bluedot - Human Disease Case & Death
-global_ebs <- data.frame()
-for (i in country){
-  url <- paste0("https://developer.bluedot.global/casecounts/locations/", i, "?startDate=", startDate, "&endDate=", endDate, "&isAggregated=false&includeSources=true&api-version=v1")
-  res <- GET(url, add_headers("Ocp-Apim-Subscription-Key" = "371e207132a2497f8e981a8d3264788b", "Cache-Control" = "no-cache")) |> content()
-  length_df <- length(res$data)
-  ebs <- data.frame()
-  for(i in 1:length_df){
-    country_name <- pluck(res, "data", i, "countryName")
-    disease_name <- pluck(res, "data", i, "diseaseName")
-    vec <- enframe(pluck(res, "data", i, "sources")) |> unnest_wider(value)
-    vec$name <- disease_name
-    vec$country <- country_name
-    ebs <- rbind(ebs, vec)
-  }
-  global_ebs <- rbind(global_ebs, ebs)
-}
+url <- paste0("https://developer.bluedot.global/casecounts/?diseaseIds=", disease, "&locationIds=", ams, "&startDate=", startDate, "&endDate=", endDate, "&isAggregated=false&includeSources=true&api-version=v1")
+res <- GET(url, add_headers("Ocp-Apim-Subscription-Key" = "52bd528ca9cd407394791ca418a7b409", "Cache-Control" = "no-cache")) |> content()
+ams_ebs <- enframe(pluck(res, "data")) |> unnest_wider(value) |> mutate(date = as.Date(reportedDate)) |> 
+  select(date, diseaseName, countryName, minSources) |> unnest(minSources) |> 
+  unnest_wider(minSources)
 
 ams_ebs <- data.frame()
 for (i in ams){
@@ -72,8 +61,8 @@ event <- rbind(event, join)
 # Bluedot - Newsfeed
 url <- paste0("https://developer.bluedot.global/daas/articles/infectious-diseases/?startDate=", startDate, "&locationIds=", ams,"&diseaseId=", disease, "&includeBody=false&includeDuplicates=false&excludeArticlesWithoutEvents=false&limit=1000&format=json&api-version=v1")
 res <- content(GET(url, add_headers("Ocp-Apim-Subscription-Key" = "ae0e017fd9b9419a927a00f3f1524edb", "Cache-Control" = "no-cache")))
-data <- enframe(pluck(res, "data")) |> unnest_wider(value) |> unnest(diseases) |> unnest(locations)
-gt(data)
+data <- enframe(pluck(res, "data")) |> unnest_wider(value) |> unnest(diseases) |> unnest(locations)|> unnest_wider(diseases, names_sep = "_") |> unnest_wider(locations, names_sep = "_")
+DT::datatable(data)
 
 |> select(sourceUrl, sourceName, publishedTimestamp, articleHeadline, diseases, locations, articleSummary)
 diseaseName <- enframe(pluck(res, "data", 10, "diseases")) |> select(-name) |> unnest_wider(value)
