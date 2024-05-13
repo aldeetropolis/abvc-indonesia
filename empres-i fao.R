295fc980e55183cf9c2d5aa072d87b9f3b3e9358
aldeetropolis <aldee.naruto@gmail.com>
2024-05-13 11:31:23 +0700

    update on empres

--- empres-i fao.R
+++ empres-i fao.R
@@ -1,3 +1,5 @@
 library(tidyverse)
 
-data <- read_csv("/Users/aldilasan/Downloads/overview-raw-data_202405060309.csv", skip = 10)
+data_overview <- read_csv("/Users/aldilasan/Downloads/overview-raw-data_202405060309.csv", skip = 10)
+
+data_epidemiology <- read_csv("/Users/aldilasan/Downloads/epidemiology-raw-data_202405130334.csv", skip = 13)
